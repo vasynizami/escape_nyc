59d574a0bb4cef4546824de88e059246994e5c89
vasynizami <victoriartz89@gmail.com>
2020-03-10 13:05:13 -0400

    added some styling

--- main.js
+++ main.js
@@ -16,43 +16,24 @@ button.addEventListener("click", async () => {
   const response = await axios.get(request);
   let res = response.data.data;
   let airport = res[Object.keys(res)[0]];
-  let flights = airport[Object.keys(airport)[0]];
-  console.log(airport);
-  console.log(flights);
-  console.log(Object.keys(airport).length);
+  let flights = airport[Object.keys(airport)];
+  // console.log(airport);
+  // console.log(flights);
+  // console.log(Object.keys(airport).length);
 
   for (let i = 0; i < Object.keys(airport).length; i += 1) {
    
-    console.log(airport[Object.keys(airport)[i]]);
+    // console.log(airport[Object.keys(airport)[i]]);
     responseDiv.innerHTML += `
       <p>Flight price in USD</p>
-      <p>${flights.price}</p>
+      <p>${airport[Object.keys(airport)[i]].price}</p>
       <p>Airline</p>
-      <p>${flights.airline}</p>
+      <p>${airport[Object.keys(airport)[i]].airline}</p>
       <p>Flight number</p>
-      <p>${flights.flight_number}</p>
+      <p>${airport[Object.keys(airport)[i]].flight_number}</p>
       <p>Flight departs</p>
-      <p>${flights.departure_at}</p>
+      <p>${airport[Object.keys(airport)[i]].departure_at}</p>
   `;
   
   }
 });
-
-// responseDiv.innerHTML += `<p>These are the cheapest flights to ${destination} from JFK this month:</p>
-//   <p>${flights.destination}</p>
-//   `
-
-//----------------------------------------------
-//--------- api for aeroports lookup  ------------
-//---------------------------------------------
-
-// const request_one = "http://aerodatabox.p.rapidapi.com/airports/search/term?limit=10&q=schiphol"
-// const api_key = {
-//  "x-rapidapi-key": "acb78ea11amsh46ce02561573e2dp1479b5jsnd98ffff069fe"
-// }
-
-// const getAirports = async () => {
-//   const response = await axios.get(request_one, api_key);
-//   debugger;
-// };
-// getAirports();
