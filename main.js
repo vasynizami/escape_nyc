ad74012354325dfd60d4cb54463819dfd54d682f
vasynizami <victoriartz89@gmail.com>
2020-03-10 13:35:34 -0400

    fixed results div css

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const button = document.querySelector("button");
 const thisMonth = new Date().toISOString().slice(0, 7);
 const api_key = "c2e79b2b6b6571705e78656293cbbc8a";
 const user_input = document.querySelector("input");
-const responseDiv = document.querySelector("div");
+const responseDiv = document.querySelector("#results");
+const theads = document.querySelector("#tableheads")
 
 //-----------------------------------------
 //----------------------------------------
@@ -11,28 +12,41 @@ const responseDiv = document.querySelector("div");
 //----------------------------------------
 
 button.addEventListener("click", async () => {
+  
   const destination = user_input.value;
   const request = `http://api.travelpayouts.com/v1/prices/cheap?origin=JFK&destination=${destination}&depart_date=${thisMonth}&token=${api_key}&currency=USD`;
   const response = await axios.get(request);
   let res = response.data.data;
   let airport = res[Object.keys(res)[0]];
-  let flights = airport[Object.keys(airport)];
-  // console.log(airport);
-  // console.log(flights);
-  // console.log(Object.keys(airport).length);
+
+  theads.innerHTML += `
+    <p>Flight price in USD</p>
+    <p>Airline</p>
+    <p>Flight number</p>
+    <p>Flight departs</p>
+    <p>Flight returns</p>
+
+  `
 
   for (let i = 0; i < Object.keys(airport).length; i += 1) {
+
+    let flights = airport[Object.keys(airport)[i]];
    
-    // console.log(airport[Object.keys(airport)[i]]);
+    // console.log(flights);
+
     responseDiv.innerHTML += `
-      <p>Flight price in USD</p>
-      <p>${airport[Object.keys(airport)[i]].price}</p>
-      <p>Airline</p>
-      <p>${airport[Object.keys(airport)[i]].airline}</p>
-      <p>Flight number</p>
-      <p>${airport[Object.keys(airport)[i]].flight_number}</p>
-      <p>Flight departs</p>
-      <p>${airport[Object.keys(airport)[i]].departure_at}</p>
+
+    <div>
+      <p>${flights.price}</p>
+      
+      <p>${flights.airline}</p>
+      
+      <p>${flights.flight_number}</p>
+      
+      <p>${flights.departure_at}</p>
+      
+      <p>${flights.return_at}</p>
+    </div>
   `;
   
   }
