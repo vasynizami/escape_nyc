5c3a37e929bbd77e4bff8bbfe369ff0cb478878b
vasynizami <victoriartz89@gmail.com>
2020-03-12 14:48:57 -0400

    added comments

--- main.js
+++ main.js
@@ -1,59 +1,66 @@
-//----------------------------------------
-//----------------------------------------
-//------------variables------------------
-//----------------------------------------
-//----------------------------------------
+//variables for API call to travelpayouts
 
 const button = document.querySelector("button");
 const thisMonth = new Date().toISOString().slice(0, 7);
+//add cors workaround from heroku before the url
+const url = "http://cors-anywhere.herokuapp.com/api.travelpayouts.com/v1/prices/cheap"
 const api_key = "c2e79b2b6b6571705e78656293cbbc8a";
 const user_input = document.querySelector("input");
 const responseDiv = document.querySelector("#results");
 const theads = document.querySelector("#tableheads");
 
-//-------------------------------------------
-//------------------------------------------
-//------get cheapest flights on click-----
-//----------------------------------------
-//----------------------------------------
+
+//getting response on click
 
 button.addEventListener("click", async () => {
   
   const destination = user_input.value;
+
+  //check that user entered an IATA code
   if (destination.length !== 3) {
-    alert("Please, enter the IATA code!")
+
+    alert("Please, enter the IATA code!");
+
   } else {
-      const request = `http://cors-anywhere.herokuapp.com/api.travelpayouts.com/v1/prices/cheap?origin=JFK&destination=${destination}&depart_date=${thisMonth}&token=${api_key}&currency=USD`;
-      const response = await axios.get(request);
-      let res = response.data.data;
-      let airport = res[Object.keys(res)[0]];
 
-      theads.innerHTML = `
+    const request = `${url}?origin=JFK&destination=${destination}&
+      depart_date=${thisMonth}&token=${api_key}&currency=USD`;
+    
+    const response = await axios.get(request);
+
+    let res = response.data.data;
+    
+    //the response from this api is object, not array, hence such syntax
+    let airport = res[Object.keys(res)[0]];
+    
+    //add headings for the displayed results
+    theads.innerHTML = `
+      <div>
+        <p>Flight price</p>
+        <p>Flight number</p>
+        <p>Flight departs</p>
+        <p>Flight returns</p>
+      <div>
+    `;
+    
+    //preparing to populate the responseDiv with results
+    //give default empty string value to responseDiv so that it resets 
+    //the search results every time the input value is changed
+    responseDiv.innerHTML = "";
+
+    for (let i = 0; i < Object.keys(airport).length; i += 1) {
+
+      let flights = airport[Object.keys(airport)[i]];
+
+      //display results
+      responseDiv.innerHTML += `
         <div>
-          <p>Flight price</p>
-          <p>Flight number</p>
-          <p>Flight departs</p>
-          <p>Flight returns</p>
-        <div>
-        `;
-  
-      //we have to give the default empty string value to responseDiv so that it resets 
-      //the search results every time
-      responseDiv.innerHTML = "";
-
-      for (let i = 0; i < Object.keys(airport).length; i += 1) {
-        let flights = airport[Object.keys(airport)[i]];
-
-        console.log(flights);
-
-        responseDiv.innerHTML += `
-          <div>
-            <p>$${flights.price}</p>
-            <p>${flights.airline} ${flights.flight_number}</p>
-            <p>${flights.departure_at.slice(0, 10)}</p>
-            <p>${flights.return_at.slice(0, 10)}</p>
-          </div>
-        `;
-      }
+          <p>$${flights.price}</p>
+          <p>${flights.airline} ${flights.flight_number}</p>
+          <p>${flights.departure_at.slice(0, 10)}</p>
+          <p>${flights.return_at.slice(0, 10)}</p>
+        </div>
+      `;
+    }
   }
 })
--- toggle.js
+++ toggle.js
@@ -1,46 +1,44 @@
+//code for sidebar API calls to openWeather and giphy
 
 
 const openWeather = async () => {
 
   const api = "a9774b2823a4f33972a6f727864cbae9";
-  const url = "http://api.openweathermap.org/data/2.5/weather?q=manhattan&";
+  const base_url = "http://api.openweathermap.org/data/2.5/weather?q=manhattan&";
   const weatherDiv = document.querySelector(".weather");
-
-  const response = await axios.get(`${url}units=imperial&appid=${api}`);
-
+  const response = await axios.get(`${base_url}units=imperial&appid=${api}`);
 
   //display results
-
   weatherDiv.innerHTML += `
-  <div>
-   <h2>Weather in NY</h2>
-   <p>${response.data.main.temp}&#8457</p>
-   <p>Wind: ${response.data.wind.speed}mph</p>
-   <p>${response.data.weather[0].description}</p>
-  </div>
+    <div>
+    <h2>Weather in NY</h2>
+    <p>${response.data.main.temp}&#8457</p>
+    <p>Wind: ${response.data.wind.speed}mph</p>
+    <p>${response.data.weather[0].description}</p>
+    </div>
   `
+  //run giphy() inside the openWeather() because it needs to use openWeather's 
+  //response results
   const giphy = async () => {
 
     const api = "fMI9TfopM0kycP7aWznKC62iGbzY0Pg7";
     const url = "http://api.giphy.com/v1/gifs/search";
     const weatherDiv = document.querySelector(".weather");
-  
     const resp = await axios.get(`${url}?q=${response.data.weather[0].description}&api_key=${api}`);
     
-    //display results
-  
+    //display result
     weatherDiv.innerHTML += `
-    
-      <img src="${resp.data.data[0].images.fixed_height.url}"/>
-   
-    `
+      <img src="${resp.data.data[0].images.fixed_height.url}"/>`
   }
   
   giphy();
+
 }
 openWeather();
 
-//open or close sidebar on clicking the plane icon
+
+//toggle sidebar on clicking the plane icon
+
 const plane = document.querySelector(".toggle_bar");
 
 plane.addEventListener("click", () => {
